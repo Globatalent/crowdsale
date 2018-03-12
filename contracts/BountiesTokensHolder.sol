be4131147ed2d880e3a08bb096aaabad5b1050d4
Iván Martín <ivanmartinga@gmail.com>
2018-03-12 10:59:16 +0100

    Early investors and bounties holders

--- contracts/BountiesTokensHolder.sol
+++ contracts/BountiesTokensHolder.sol
@@ -1,40 +1,5 @@
 pragma solidity ^0.4.18;
 
-/*
-    Copyright 2017, Jordi Baylina
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/// @title BountiesTokensHolder Contract
-/// @author Jordi Baylina
-/// @dev This contract will hold the tokens of the developers.
-///  Tokens will not be able to be collected until 3 months after the contribution ends
-
-
-//  collectable tokens
-//   |
-//   |
-//   |          .______  vestedTokens rect
-//   |        . |
-//   |      .   |
-//   |    .     |
-//   +===+======+------------> time
-//     Contrib  3 Months
-//       End
-
-
 import "./MiniMeToken.sol";
 import "./TokenContribution.sol";
 import "./SafeMath.sol";
@@ -58,7 +23,7 @@ contract BountiesTokensHolder is Owned {
     function collectTokens() public onlyOwner {
         uint256 finalizedTime = contribution.finalizedTime();
 
-        require(collectedTokens == 0 && finalizedTime > 0 && getTime() > finalizedTime.add(months(3)));
+        require(finalizedTime > 0 && getTime() > finalizedTime);
 
         uint256 balance = miniMeToken.balanceOf(address(this));
 
